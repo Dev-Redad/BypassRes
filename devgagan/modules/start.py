7fb06e2d45dbbfef8b26725797f28c16b8da53b6
Dev-Redad <tuiojabe@gmail.com>
2025-05-06 09:32:37 +0530

    Update start.py

--- devgagan/modules/start.py
+++ devgagan/modules/start.py
@@ -183,7 +183,7 @@ async def terms(client, message):
     buttons = InlineKeyboardMarkup(
         [
             [InlineKeyboardButton("📋 See Plans", callback_data="see_plan")],
-            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/kingofpatal")],
+            [InlineKeyboardButton("💬 Contact Now", url="https://t.me/goat_me")],
         ]
     )
     await message.reply_text(terms_text, reply_markup=buttons)
